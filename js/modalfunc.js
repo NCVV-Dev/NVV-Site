cfb78d7645de8efc73fb43032f9f8ab875bab6a6
harmony <61928705+harmonyzt@users.noreply.github.com>
2022-02-16 23:12:56 +0500

    Removed commented out functions

--- js/modalfunc.js
+++ js/modalfunc.js
@@ -5,22 +5,16 @@ var span = document.getElementsByClassName("close")[0];
 var overlay = document.getElementById("modal__overlay");
 
 btn.onclick = function() {
-  //modal.style.display = "block";
-  //overlay.style.display = "block";
   $("#upl__modal").slideToggle(500);
   $("#modal__overlay").slideToggle(500);
 }
 
 nofound.onclick = function() {
-  //modal.style.display = "block";
-  //overlay.style.display = "block";
   $("#upl__modal").slideToggle(500);
   $("#modal__overlay").slideToggle(500);
 }
 
 span.onclick = function() {
-   // modal.style.display = "none";
-   //overlay.style.display = "none";
    $("#upl__modal").slideToggle(500);
    $("#modal__overlay").slideToggle(500);
 }
@@ -29,7 +23,5 @@ window.onclick = function(event) {
   if (event.target == modal) {
       $("#upl__modal").slideToggle(500);
       $("#modal__overlay").slideToggle(500);
-      //modal.style.display = "none";
-      //overlay.style.display = "none";
   }
 } 
