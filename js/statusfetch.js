4952759e829b1fc37bb3fc3370c7ee43d824eefa
harmony <61928705+harmonyzt@users.noreply.github.com>
2022-04-09 17:07:59 +0500

    Added information box and response time on the main page

--- js/statusfetch.js
+++ js/statusfetch.js
@@ -1,6 +1,23 @@
 getText("fetch_outageinfo.txt");
 async function getText(file) {
-  let myObject = await fetch(file);
-  let myText = await myObject.text();
-  document.getElementById("outageinfdyn").innerHTML = myText;
+  let FetchObj = await fetch(file);
+  let FetchTxt = await FetchObj.text();
+  document.getElementById("outageinfdyn").innerHTML = FetchTxt;
 }
+
+window.fetch = (function(fetch) {
+  return function(fn, t) {
+    const begin = Date.now();
+    return fetch.apply(this, arguments).then(function(response) {
+      response.timing = Date.now() - begin;
+      return response;
+    });
+  };
+})(window.fetch);
+
+fetchtime__d = document.getElementById('response');
+
+fetch('/').then(function(response) {
+    fetchtime__d.insertAdjacentHTML('beforebegin', response.timing + 'ms');
+    return response.json();
+})
