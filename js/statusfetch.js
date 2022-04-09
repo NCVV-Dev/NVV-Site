091128afd2b04954d391494c71e694b4bfb3dcdd
harmony <61928705+harmonyzt@users.noreply.github.com>
2022-04-09 15:01:40 +0500

    Response time test

--- js/statusfetch.js
+++ js/statusfetch.js
@@ -4,3 +4,23 @@ async function getText(file) {
   let myText = await myObject.text();
   document.getElementById("outageinfdyn").innerHTML = myText;
 }
+
+window.fetch = (function(fetch) {
+  return function(fn, t) {
+    const begin = Date.now();
+    return fetch.apply(this, arguments).then(function(response) {
+      response.timing = Date.now() - begin;
+      return response;
+    });
+  };
+})(window.fetch);
+
+fetch('https://github.com')
+  .then(function(response) {
+    console.log(response.timing + 'ms')
+    return response.json();
+  }).then(function(jsonData) {
+    console.log(jsonData);
+  }).catch(function(err) {
+    console.log("Opps, Something went wrong!", err);
+  })
