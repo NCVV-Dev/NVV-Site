946316935eab58391538c920745b028cc52c932b
harmony <61928705+harmonyzt@users.noreply.github.com>
2022-05-11 18:40:40 +0500

    Termination of mods page. Let's keep the actual meaning of this project 👋

--- js/welcomepagefunc.js
+++ js/welcomepagefunc.js
@@ -1,1 +1,18 @@
-const wlcoverlay=document.querySelector(".welcome_overlay");if(getCookie("FirstTime")!=false){wlcoverlay.style.opacity=0;wlcoverlay.style.display="none"}else{wlcoverlay.style.opacity=1;wlcoverlay.style.display="block"};function WelcomeButtonAction(){setTimeout(function(){wlcoverlay.style.opacity=0;setCookie("FirstTime","false",365)},500);setTimeout(function(){wlcoverlay.style.display="none"},1100)};function WelcomeButtonRedirect(){window.location.replace("/mods/");setCookie("FirstTime","false",365)}
+const wlcoverlay = document.querySelector(".welcome_overlay");
+if (getCookie("FirstTime") != false) {
+    wlcoverlay.style.opacity = 0;
+    wlcoverlay.style.display = "none"
+} else {
+    wlcoverlay.style.opacity = 1;
+    wlcoverlay.style.display = "block"
+};
+
+function WelcomeButtonAction() {
+    setTimeout(function () {
+        wlcoverlay.style.opacity = 0;
+        setCookie("FirstTime", "false", 365)
+    }, 500);
+    setTimeout(function () {
+        wlcoverlay.style.display = "none"
+    }, 1100)
+};
