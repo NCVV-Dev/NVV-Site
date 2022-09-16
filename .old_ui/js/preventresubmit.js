6c32fd3475dfa5c4fcc7ce2638d3a539c8f13f11
harmony <hattrix@yandex.ru>
2022-09-17 01:30:47 +0500

    Remove old UI

--- .old_ui/js/preventresubmit.js
+++
@@ -1,1 +0,0 @@
-if(window.history.replaceState){window.history.replaceState(null,null,window.location.href)}
--- .old_ui/js/sidemenufunc.js
+++
@@ -1,1 +0,0 @@
-let sidebar=document.querySelector(".sidebar");let closeBtn=document.querySelector("#btn");closeBtn.addEventListener("click",()=>{sidebar.classList.toggle("open");menuBtnChange()});function menuBtnChange(){if(sidebar.classList.contains("open")){closeBtn.classList.replace("bx-menu","bx-menu-alt-right")}else{closeBtn.classList.replace("bx-menu-alt-right","bx-menu")}}
