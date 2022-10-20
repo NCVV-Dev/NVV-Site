d74ea1571cb68cd1d09cd0b8897413a571bf677d
harmony <hattrix@yandex.ru>
2022-10-20 22:18:45 +0500

    Add notification system to shuffle button

--- js/shufflecontent.js
+++ js/shufflecontent.js
@@ -1,10 +1,26 @@
+var t = document.getElementById("nf__popup");
+
 function switchrandomizing(){
     if(getCookie("EnableShuffle") == "true"){
         setCookie("EnableShuffle", "false", 365);
         localStorage.setItem('enableshuffle', 'false');
+
+        t.style.color="var(--warn)";
+        t.innerHTML =  "Visual config shuffle disabled";
+        t.className = "show";
+        setTimeout(function(){ 
+            t.className = t.className.replace("show", "");
+        }, 3000);
     } else {
         setCookie("EnableShuffle", "true", 365);
         localStorage.setItem('enableshuffle', 'true');
+        
+        t.style.color="var(--buttonsubmitbg)";
+        t.innerHTML =  "Visual config shuffle enabled!";
+        t.className = "show";
+        setTimeout(function(){ 
+            t.className = t.className.replace("show", "");
+        }, 3000);
     }
 }
 
@@ -16,6 +32,13 @@ $(function() {
       return 0.5 - Math.random();
     });
     parent.append(divs);
+
+    t.style.color="var(--buttonsubmitbg)";
+    t.innerHTML =  "Shuffling visual configs...";
+    t.className = "show";
+    setTimeout(function(){ 
+        t.className = t.className.replace("show", "");
+    }, 3000);
 } else {
     localStorage.setItem('enableshuffle', 'false');
     setCookie("EnableShuffle", "false", 365)
