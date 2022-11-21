11d453555840700f58d7869cd548a14a125c1d0f
harmony <hattrix@yandex.ru>
2022-11-22 00:00:47 +0500

    [FWARE] Separately store needed JS scripts

---
+++ fedoraware/js/loaderfunc.js
@@ -0,0 +1,38 @@
+(function() {
+    function id(v) {
+        return document.getElementById(v)
+    }
+
+    function loadbar() {
+        let prog = id("progress"),
+            img = document.images,
+            c = 0,
+            tot = img.length;
+        if (tot == 0) {
+            return doneLoading()
+        }
+
+        function imgLoaded() {
+            c += 1;
+            let perc = ((100 / tot * c) << 0) + "%";
+            prog.style.width = perc;
+            if (c === tot) {
+                return doneLoading()
+            }
+        }
+
+        function doneLoading() {
+            setTimeout(function() {
+                prog.style.opacity = "none";
+                prog.style.opacity = 0
+            }, 1800)
+        }
+        for (let i = 0; i < tot; i += 1) {
+            let tImg = new Image();
+            tImg.onload = imgLoaded;
+            tImg.onerror = imgLoaded;
+            tImg.src = img[i].src
+        }
+    }
+    document.addEventListener('DOMContentLoaded', loadbar, false)
+}());
---
+++ fedoraware/js/uploadhandler.js
@@ -0,0 +1,23 @@
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+$(function() {
+    let t = document.getElementById("nf__popup");
+
+    if(getCookie("submit") == 'success'){
+        t.style.color="var(--buttonsubmitbg)";
+        t.innerHTML =  "Visual config submitted!";
+        t.className = "show";
+    } else if(getCookie("submit") == 'fail') {
+        t.style.color="var(--warn)";
+        t.innerHTML = "Something went wrong :(";
+        t.className = "show";
+    }
+
+    eraseCookie("submit");
+
+    setTimeout(function(){ 
+        t.className = t.className.replace("show", "");
+    }, 3000);
+})
