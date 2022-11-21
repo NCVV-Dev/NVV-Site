db3d0443ff13c6ded08ccca31e65a7365d900c62
harmony <hattrix@yandex.ru>
2022-11-22 00:13:53 +0500

    Fix not working PHP and JS

--- fedoraware/js/downloads_manager.js
+++ fedoraware/js/downloads_manager.js
@@ -8,9 +8,9 @@ document.querySelectorAll('.dwn__count').forEach(elem => {
 let icon = document.createElement("em");
 const popular = document.createElement('div');
 popular.textContent = 'Popular!';
-popular.style = "position: absolute; font-size: 13px; text-align: center; color: let(--buttonhover); font-weight: 600;";
+popular.style = "position: absolute; font-size: 13px; text-align: center; color: var(--buttonhover); font-weight: 600;";
 icon.className = "bx bxs-hot tooltip";
-icon.style = "color: let(--buttonhover);";
+icon.style = "color: var(--buttonhover);";
 
 document.querySelectorAll('.dwn__count').forEach(elem => {
   if (parseInt(elem.innerText.trim()) == highestNumber)
