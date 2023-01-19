4fc93f19baa309871b8cf9fb8c93f1e0fcd19387
harmony <hattrix@yandex.ru>
2023-01-19 06:03:14 +0500

    Sort out stuff from navbar to it's places

--- js/main.js
+++ js/main.js
@@ -117,7 +117,6 @@ document.querySelectorAll('.dwn__count').forEach(elem => {
 let icon = document.createElement("em");
 const popular = document.createElement('div');
 popular.textContent = 'Popular!';
-popular.style = "position: absolute; font-size: 13px; text-align: center; color: var(--buttonhover); font-weight: 600;";
 icon.className = "bx bxs-hot tooltip";
 icon.style = "color: var(--buttonhover);";
 
@@ -371,3 +370,79 @@ $(function() {
     setCookie("EnableShuffle", "false", 365)
 }
 });
+
+
+class Fun {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
+const phrases = [
+  'No advertisements.',
+  'No any kind of transactions.',
+  'No popular trackers.',
+  'Just for people.',
+  'Completely free.'
+]
+
+const el = document.querySelector('.text')
+const fx = new Fun(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
