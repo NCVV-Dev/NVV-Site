6a63b1e7f500cfc93b9a0be5ed45da9588c7d56e
harmony <hattrix@yandex.ru>
2023-02-03 07:21:23 +0500

    Revert changes

--- js/checkformobile.js
+++ js/checkformobile.js
@@ -1,5 +1,3 @@
 if (navigator.userAgent.toLowerCase().match(/mobile/i)) {
-    if (location.search.indexOf('app=true') > -1) {
-        document.write('<link rel="stylesheet" type="text/css" href="https://mobile.ncvisualsvault.cc/ncv.css" />');
-      }
+  window.location.href = "https://mobile.ncvisualsvault.cc/"
 }
