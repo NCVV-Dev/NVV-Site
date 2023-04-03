d4a740e2c538c9cedad35f5bbf9d6e6c085fd2e8
harmony <hattrix@yandex.ru>
2023-04-03 11:09:38 +0500

    Test commit

--- js/checkformobile.js
+++ js/checkformobile.js
@@ -1,4 +1,4 @@
-// This is probably the easiest way to track mobile devices and redirect them to more usable website version
+
 if (navigator.userAgent.toLowerCase().match(/mobile/i)) {
   window.location.href = "https://mobile.ncvisualsvault.cc/"
 }
