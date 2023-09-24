0a7157f2e6adf9df4c932d4b59ba1d8fc2fbce8b
harmony <hattrix@yandex.ru>
2023-09-24 19:45:08 +0500

    Format notification function properly

--- js/main.js
+++ js/main.js
@@ -167,21 +167,21 @@ let t = document.getElementById("nf__popup");
 if (window.location.pathname == '/') {
     // Notification system for uploading
     $(function () {
-            if (getCookie("submit") == 'success') {
-                t.style.color = "var(--buttonsubmitbg)";
-                t.innerHTML = "Visual config submitted.";
-                t.className = "show";
-                eraseCookie("submit");
-            } else if (getCookie("submit") == 'fail') {
-                t.style.color = "var(--warn)";
-                t.innerHTML = "Failed to submit your visual config :(";
-                t.className = "show";
-                eraseCookie("submit");
-            }
+        if (getCookie("submit") == 'success') {
+            t.style.color = "var(--buttonsubmitbg)";
+            t.innerHTML = "Visual config submitted.";
+            t.className = "show";
+            eraseCookie("submit");
+        } else if (getCookie("submit") == 'fail') {
+            t.style.color = "var(--warn)";
+            t.innerHTML = "Failed to submit your visual config :(";
+            t.className = "show";
+            eraseCookie("submit");
+        }
 
-            setTimeout(function () {
-                t.className = t.className.replace("show", "");
-            }, 3000);
+        setTimeout(function () {
+            t.className = t.className.replace("show", "");
+        }, 3000);
     });
 }
 
