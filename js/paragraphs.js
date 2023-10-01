0f50e68f9f237fe763921e1b9aa35d0f3987dd8d
harmony <hattrix@yandex.ru>
2023-10-01 07:42:07 +0500

    Improve paragraphs.js

--- js/paragraphs.js
+++ js/paragraphs.js
@@ -3,5 +3,19 @@ $(function () {
         let h = $(this).attr('id')
         h.href = h;
         document.location.href = '#' + h;
+
+        let paragraph = $(this)
+
+        if ($(paragraph).hasClass('gd_smaller')) {
+            $(this).attr('style', 'transition: all 400ms ease-in-out; color: var(--buttonsubmithover);');
+            setTimeout(function () {
+                paragraph.removeAttr('style')
+            }, 1500);
+        } else {
+            $(this).attr('class', 'paragraph_active');
+            setTimeout(function () {
+                paragraph.removeAttr('class')
+            }, 1500);
+        }
     });
 });
