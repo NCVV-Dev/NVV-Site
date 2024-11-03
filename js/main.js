54022e3734f6c49cdde9784a6d8aed9542ca9952
harmony <ncvisualsvault@protonmail.com>
2024-11-03 18:14:11 +0300

    Remove text spam at the welcome page

--- js/main.js
+++ js/main.js
@@ -134,8 +134,6 @@ function WelcomeButtonAction() {
 
 // Showcase random image for welcome page
 // NOTE: Loads after website images 
-let prevNumber
-let number
 
 function setRandomImage() {
     const ImageArray = [
@@ -149,13 +147,7 @@ function setRandomImage() {
         "/media/menus/hrmmenu.png"
     ];
 
-    // To prevent same image from displaying
-    if (prevNumber = number) {
-        //do nothing :/
-    }
-
-    number = Math.floor(Math.random() * ImageArray.length);
-    prevNumber = number;
+    let number = Math.floor(Math.random() * ImageArray.length);
 
     return document.getElementById("randomimage").innerHTML = '<img src="' + ImageArray[number] + '" />';
 }
@@ -292,98 +284,6 @@ function ShuffleVisuals() {
         setCookie("EnableShuffle", "false", 365)
     }
 };
-
-
-// Text Animation on the Welcome Page
-if (getCookie("FirstTime") != "false") {
-    class Randchar {
-        constructor(el) {
-            this.el = el
-            this.chars = '!<>-_\\/[]{}—=+*^?#________'
-            this.update = this.update.bind(this)
-        }
-
-        setText(newText) {
-            const oldText = this.el.innerText
-            const length = Math.max(oldText.length, newText.length)
-            const promise = new Promise((resolve) => this.resolve = resolve)
-            this.queue = []
-            for (let i = 0; i < length; i++) {
-                const from = oldText[i] || ''
-                const to = newText[i] || ''
-                const start = Math.floor(Math.random() * 40)
-                const end = start + Math.floor(Math.random() * 40)
-                this.queue.push({
-                    from,
-                    to,
-                    start,
-                    end
-                })
-            }
-            cancelAnimationFrame(this.frameRequest)
-            this.frame = 0
-            this.update()
-            return promise
-        }
-
-        update() {
-            let output = ''
-            let complete = 0
-            for (let i = 0, n = this.queue.length; i < n; i++) {
-                let {
-                    from,
-                    to,
-                    start,
-                    end,
-                    char
-                } = this.queue[i]
-                if (this.frame >= end) {
-                    complete++
-                    output += to
-                } else if (this.frame >= start) {
-                    if (!char || Math.random() < 0.28) {
-                        char = this.randomChar()
-                        this.queue[i].char = char
-                    }
-                    output += `<span class="dud">${char}</span>`
-                } else {
-                    output += from
-                }
-            }
-
-            this.el.innerHTML = output
-            if (complete === this.queue.length) {
-                this.resolve()
-            } else {
-                this.frameRequest = requestAnimationFrame(this.update)
-                this.frame++
-            }
-        }
-        randomChar() {
-            return this.chars[Math.floor(Math.random() * this.chars.length)]
-        }
-    }
-
-    const phrases = [
-        'No advertisements.',
-        'No popular trackers.',
-        'Just for people.',
-        'Completely free.'
-    ]
-
-    const el = document.querySelector('.text')
-    const fx = new Randchar(el)
-
-    let counter = 0
-    const next = () => {
-        fx.setText(phrases[counter]).then(() => {
-            setTimeout(next, 800)
-        })
-        counter = (counter + 1) % phrases.length
-    }
-
-    next()
-}
 
 // Sort visual configs by name
 function sortbyname() {
